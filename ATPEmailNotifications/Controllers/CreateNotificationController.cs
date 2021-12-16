777421c2b879139703fe014364bc295d640260b9
Plaugher <jplaughe@hdrinc.com>
2021-12-16 15:20:04 -0800

    initial commit. SendGrid is working.

---
+++ ATPEmailNotifications/Controllers/CreateNotificationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ATPEmailNotifications
+{
+    [Route("api/NewNotif")]
+    [ApiController]
+    public class CreateNotificationController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public CreateNotificationController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpPost]
+        public string Post([FromForm] string recipientName, [FromForm] string recipientEmail, [FromForm] string initiatorName, [FromForm] string initiatorEmail, [FromForm] string message)
+        {
+            System.Console.WriteLine($"the list of params in controller: {recipientName}, {recipientEmail}, {initiatorName}, {initiatorEmail}, {message}");
+            var notification = new EmailNotification(config);
+            var msg = notification.NewEmail(recipientName, recipientEmail, initiatorName, initiatorEmail, message);
+            string othermsg = "testing";
+            return othermsg;
+        }
+    }
+}
---
+++ ATPEmailNotifications/Model/Email.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace ATPEmailNotifications
+{
+    public class EmailNotification
+    {
+        private readonly IConfiguration config;
+
+        public EmailNotification(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public async Task<string> NewEmail(string recipientName, string recipientEmail, string initiatorName, string initiatorEmail, string message)
+        {
+            var client = new SendGridClient(this.config["OneViewROW_apiKey"]);
+            var from = new EmailAddress(initiatorEmail, initiatorName);
+            var subject = "Testing Email API";
+            var to = new EmailAddress(recipientEmail, recipientName);
+            var textNotificationMessage = message;
+            var htmlnotificationMessage = $"<strong>{message}</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, textNotificationMessage, htmlnotificationMessage);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+            System.Console.WriteLine($"Response: {response.StatusCode}");
+            return $"Attempted email result: {response.StatusCode}";
+        }
+    }
+}
